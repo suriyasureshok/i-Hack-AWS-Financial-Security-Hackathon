da04d9fe0d2e0772bfa9861474bdc84b82b1c026
Suriya S <240171.ad@rmkec.ac.in>
2025-01-16 11:53:42 +0530

    Create App.js

---
+++ web-interface/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FileUpload from './components/FileUpload';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Fraud Detection System</h1>
+      <FileUpload />
+    </div>
+  );
+};
+
+export default App;
